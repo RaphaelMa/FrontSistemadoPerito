8a864493263cabff0cece2e5cabc3818da07e108
Maik Junior <maik.designer101@gmail.com>
2022-04-27 22:08:00 -0300

    change design plans

--- src/Pages/Plans/Plans.tsx
+++ src/Pages/Plans/Plans.tsx
@@ -81,8 +81,11 @@ const Plans: React.FC = () => {
   const data = buildData({ prices: internal_yearly ? YEARLY_PRICE : MONTHLY_PRICE })
   const columns = useColumns({ yearly: internal_yearly, handleClickPlan })
 
+  console.log('data', data);
+  console.log('columns', columns);
+
   return (
-    <>
+    <Content>
       <Spin spinning={loading || saving}>
         <Container>
           <Header>
@@ -105,16 +108,20 @@ const Plans: React.FC = () => {
               <Bnaaer>
                 <h2>START</h2>
               </Bnaaer>
-              <p style={{ textAlign: 'left', marginTop: '50px', fontSize: '15px', marginLeft: '10%', color: 'white' }}>
-                <p style={{ textAlign: 'center', fontSize: '25px' }}>R$ 89,90</p>
-                1 Usuário <br />
-                10 Push Free <br />
-                Dashboard <br />
-                Gestão de Perícias e Assistências <br />
-                Gestão da Agenda <br />
-                Cadastros <br />
-                Relatórios <br />
-              </p>
+              <ContainerPriceCard>
+                <div className="price">
+                  R$ {internal_yearly ? '76,40' : '89,90'}
+                </div>
+                <ul>
+                  <li>1 Usuário</li>
+                  <li>10 Push Free</li>
+                  <li>Dashboard</li>
+                  <li>Gestão de Perícias e Assistências</li>
+                  <li>Cadastros</li>
+                  <li>Gestão da Agenda</li>
+                  <li>Relatórios</li>
+                </ul>
+              </ContainerPriceCard>
               <ButtonStyle onClick={() => handleClickPlan('BASIC')}>
                 Contratar
               </ButtonStyle>
@@ -124,18 +131,20 @@ const Plans: React.FC = () => {
               <Bnaaer>
                 <h2>BASIC</h2>
               </Bnaaer>
-              <p style={{ textAlign: 'left', marginTop: '50px', fontSize: '15px', marginLeft: '10%', color: 'white' }}>
-                <p style={{ textAlign: 'center', fontSize: '25px' }}>R$ 179,90</p>
-                2 Usuários <br />
-                20 Push Free <br />
-                Dashboard <br />
-                Gestão de Perícias e Assistências <br />
-                Gestão da Agenda <br />
-                Cadastros <br />
-                Relatórios <br />
-                Gestão de Tarefas <br />
-                Anexo de Arquivos <br />
-              </p>
+              <ContainerPriceCard>
+                <div className="price">R$ {internal_yearly ? '152,90' : '179,90'}</div>
+                <ul>
+                  <li>2 usuários</li>
+                  <li>20 Push Free</li>
+                  <li>Dashboard</li>
+                  <li>Gestão de Perícias e Assistências</li>
+                  <li>Gestão da Agenda</li>
+                  <li>Cadastros</li>
+                  <li>Relatórios</li>
+                  <li>Gestão de Tarefas</li>
+                  <li>Anexo de Arquivos</li>
+                </ul>
+              </ContainerPriceCard>
               <ButtonStyle onClick={() => handleClickPlan('START')}>
                 Contratar
               </ButtonStyle>
@@ -145,21 +154,23 @@ const Plans: React.FC = () => {
               <Bnaaer>
                 <h2>PRO</h2>
               </Bnaaer>
-              <p style={{ textAlign: 'left', marginTop: '50px', fontSize: '15px', marginLeft: '10%', color: 'white' }}>
-                <p style={{ textAlign: 'center', fontSize: '25px' }}>R$ 269,90</p>
-                4 Usuários <br />
-                40 Push Free <br />
-                Dashboard <br />
-                Gestão de Perícias e Assistências <br />
-                Gestão da Agenda <br />
-                Cadastros <br />
-                Relatórios <br />
-                Gestão de Tarefas <br />
-                Anexo de Arquivos <br />
-                Gestão Financeira <br />
-                Gestão de Prazos <br />
+              <ContainerPriceCard>
+                <div className="price">R$ {internal_yearly ? '229,40' : '269,90'}</div>
+                <ul>
+                  <li>4 Usuários</li>
+                  <li>40 Push Free</li>
+                  <li>Dashboard</li>
+                  <li>Gestão de Perícias e Assistências</li>
+                  <li>Gestão da Agenda</li>
+                  <li>Cadastros</li>
+                  <li>Relatórios</li>
+                  <li>Gestão de Tarefas</li>
+                  <li>Anexo de Arquivos</li>
+                  <li>Gestão Financeira</li>
+                  <li>Gestão de Prazos</li>
+                </ul>
                 {/* Criação de Documentos "EM BREVE" <br /> */}
-              </p>
+              </ContainerPriceCard>
               <ButtonStyle onClick={() => handleClickPlan('PRO')}>
                 Contratar
               </ButtonStyle>
@@ -169,54 +180,71 @@ const Plans: React.FC = () => {
       </Spin>
 
       <PaymentMethodModal ref={PaymentMethodRef} />
-    </>
+    </Content>
   )
 }
 
 export default Plans
 
-const Container = styled.div`
-  padding: 0 15%;
-  height: 100%;
+const Content = styled.div`
   overflow-y: auto;
+  overflow-x: hidden;
+  padding: 20px 40px;
+  height: calc(100% - 5rem);
 `;
-
-const ButtonStyle = styled.button`
-  display: inline;
-  position: absolute;
-  margin-top: 32%;
-  margin-left: 1%;
-  width: 20%;
-  height: 34px; 
-  color: white;
-
-  background-color: #3CA49E;
-  border-top: none;
-  border-bottom: none;
-  border-left: none;
-  border-right: none;
-  border-radius: 10px;
-
-  transition: all 0.5s;
-
-  &:hover {
-    cursor: pointer;
-    width: 20%;
-    height: 31px;
+const Container = styled.div`
+  overflow-y: auto;
+  overflow-x: hidden;
+  padding: 20px 40px;
+  height: calc(100% - 5rem);
+`;
+const ContainerPriceCard = styled.div`
+  color: #fff;
+  padding: 16px;
+  text-align: center;
+  ul li{
+    padding: 8px 0;
+    border-bottom: 1px solid #fff;
+    list-style: none;
+    text-transform: uppercase;
+  }
+  div.price{
+    font-size: 25px;
+    font-weight: 900;
+    margin-bottom: 12px;
   }
 `;
 
-const Bnaaer = styled.div`
-  width: 21.8%;
-  margin-top: 1px;
-  border-radius: 8px;
-  height: 40px;
-  position: absolute;
-  color: white;
-  background-color: #3CA49E;
+const ButtonStyle = styled.button`
+  color: #FFFFFF;
+    font-size: 14px;
+    font-weight: 600;
+    text-transform: uppercase;
+    background-color: transparent;
+    background-image: linear-gradient(220deg, #00BED0 30%, #00D9BB 76%);
+    border-radius: 25px 25px 25px 25px;
+    padding: 10px 25px 10px 25px;
+    cursor: pointer;
+    border: none;
+    width: 150px;
+    margin: 0 auto;
+    margin-bottom: 14px;
+`;
 
+const Bnaaer = styled.div`
+  padding: 10px 10px 10px 10px;
+  background: #629ab673;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px 10px 0 0;
   h2 {
-    color: white;
+    color: #FFFFFF;
+    font-size: 20px;
+    font-weight: 800;
+    margin: 0;
+    align-self: center;
     text-align: center;
   }
 `;
@@ -231,19 +259,21 @@ const GridContainer = styled.div`
   gap: 10px;
   background-color: white;
   padding: 10px;
+  justify-content: center;
+  align-items: flex-start;
 `;
 
 const StyledCardPlan = styled.div`  
   display: flex !important;
   width: 280px;
-  height: 395px;
-  background-color: #36bdc1;
+  background-color: transparent;
+  background-image: linear-gradient(180deg, #14DDCA 0%, #00ADB9 100%);
   //box-shadow: 0 0px 7px rgba(53, 58, 65, 0.2);
   border-radius: 10px;
   justify-content: space-between;
   transition: box-shadow .3s;
   margin-bottom: 10%;
-
+  flex-direction: column;
   &:hover {
     cursor: pointer;
     box-shadow: 0 0px 30px #1793ae7e;
